c5c4d7a9d1329d78efa3577a1a1756428a44309a
Oishin Smith <oishin.smith@gmail.com>
2023-10-11 22:07:51 +0100

    adding in ajax requests and database setup

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,15 +1,27 @@
 import { Injectable } from '@angular/core';
+import { CosmosClient } from '@azure/cosmos';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
-
   private isAuthenticated = false;
+  private client: CosmosClient; // Declare the client property
+
+  constructor() {
+    // Initialize the CosmosClient instance in the constructor
+    this.client = new CosmosClient({
+      endpoint: "https://owensmith.documents.azure.com:443/",
+      key: "uT5PSIYmJtsV6S9BTkvMfiXvTgevf1XoaJoRU812hZSKfNwLfiOkCHwY6JSXagFnbtQRt04nDaWoACDbkWsPiQ=="
+    });
+    console.log(this.client);
+  }
 
   login(username: string, password: string): boolean {
+    // username and password will be passed to this function from the
+    this.queryItems();
+
     // Implement your authentication logic here (e.g., check username and password).
     if (username === 'user' && password === 'password') {
       this.isAuthenticated = true;
@@ -22,4 +34,27 @@ export class AuthService {
   isLoggedIn(): boolean {
     return this.isAuthenticated;
   }
+
+  async queryItems(): Promise<any[]> {
+    // Specify your database and container names
+    const databaseId = "owenDatabase";
+    const containerId = "items";
+
+    // Specify the SQL query
+    const query = "SELECT * FROM c";
+
+    // Query the items
+    const container = this.client.database(databaseId).container(containerId);
+    const { resources } = await container.items.query(query).fetchAll();
+    console.log(resources);
+    return resources;
+  }
 }
+
+async function main() {
+  // The rest of the README samples are designed to be pasted into this function body
+}
+
+main().catch((error) => {
+  console.error(error);
+});
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../auth.service';
+import { CosmosClient } from '@azure/cosmos';
+
+
+const endpoint = "https://your-account.documents.azure.com";
+const key = "<database account masterkey>";
+const client = new CosmosClient({ endpoint, key });
 
 @Component({
   selector: 'app-login',
@@ -13,6 +19,9 @@ export class LoginComponent {
   constructor(private authService: AuthService) {}
 
   onSubmit() {
+    // once clicked, the authService (which I created) has a function called login.
+    // you need to pass it a username and a password
+    console.log("logging in...");
     if (this.authService.login(this.username, this.password)) {
       // Redirect or show a success message.
     } else {
@@ -20,3 +29,11 @@ export class LoginComponent {
     }
   }
 }
+
+async function main() {
+  // The rest of the README samples are designed to be pasted into this function body
+}
+
+main().catch((error) => {
+  console.error(error);
+});
